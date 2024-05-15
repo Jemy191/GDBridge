0eb0210b1bd62735b2411cb4cd3ba087befc2cc5
Megan <108483531+Meythulhu@users.noreply.github.com>
2024-05-14 19:30:21 -0700

    Sanitize function parameter names from any reserved keywords, Add overload function generation for default parameters (#5)

--- GDBridge.Generator/GDBridge.Generator/BridgeWriter.cs
+++ GDBridge.Generator/GDBridge.Generator/BridgeWriter.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 using GDParser;
+using Microsoft.CodeAnalysis.CSharp;
 using SourceGeneratorUtils;
 
 namespace GDBridge.Generator;
@@ -42,35 +44,81 @@ class BridgeWriter
 
     public SourceWriter Functions(IEnumerable<GdFunction> functions)
     {
+        var scriptFunctions = new List<GdFunction>();
+
+        // Iterate over functions with default parameters omitted
         foreach (var function in functions)
         {
+            // Filter out functions which start with an underscore to avoid native function overrides
             if (function.Name.StartsWith("_"))
+            {
                 continue;
-            
+            }
+
+            var defaultParams = function.Parameters.Where(p => p.Name.Contains('=')).ToList();
+            if (!defaultParams.Any())
+            {
+                scriptFunctions.Add(function);
+                continue;
+            }
+            var nonDefaults = function.Parameters.Where(p => !defaultParams.Contains(p)).ToList();
+
+            // Add a new function for each parameter signature
+            for (var i = 0; i <= defaultParams.Count; i++)
+            {
+                var @params = new List<GdVariable>(nonDefaults);
+                @params.AddRange(defaultParams.Take(i));
+                scriptFunctions.Add(new GdFunction(function.Name, new ReadOnlyCollection<GdVariable>(@params),
+                    function.ReturnType));
+            }
+        }
+
+        foreach (var function in scriptFunctions)
+        {
             source.WriteEmptyLines(1)
-                .WriteLine($"""public {function.ReturnType.ToCSharpTypeString(availableTypes)} {Pascalize(function.Name)}({InParameters(function.Parameters)}) => GdObject.Call("{function.Name}"{CallParameters(function.Parameters)}){GetTypeCast(function.ReturnType)};""");
+                .WriteLine(
+                    $"""public {function.ReturnType.ToCSharpTypeString(availableTypes)} {Pascalize(function.Name)}({InParameters(function.Parameters)}) => GdObject.Call("{function.Name}"{CallParameters(function.Parameters)}){GetTypeCast(function.ReturnType)};""");
         }
 
         return source;
     }
     string InParameters(IEnumerable<GdVariable> parameters) => string.Join(", ", parameters.Select(InParameter));
-    string InParameter(GdVariable parameter) => $"{parameter.Type.ToCSharpTypeString(availableTypes)} {Pascalize(parameter.Name)}";
+    string InParameter(GdVariable parameter) => $"{parameter.Type.ToCSharpTypeString(availableTypes)} {Pascalize(SanitizeParameter(parameter.Name))}";
     string Pascalize(string text) => configuration.UsePascalCase ? Regex.Replace(text, "(?:^|_| +)(.)", match => match.Groups[1].Value.ToUpper()) : text;
+
+    string SanitizeParameter(string parameter)
+    {
+        // Return parameter if it's already valid
+        if (SyntaxFacts.GetKeywordKind(parameter) == SyntaxKind.None && SyntaxFacts.IsValidIdentifier(parameter))
+        {
+            return parameter;
+        }
+
+        // Remove default parameter values
+        var param = parameter;
+        if (parameter.Contains('='))
+        {
+            param = parameter.Substring(0, parameter.IndexOf('=')).Trim();
+        }
+
+        // Validate it's not a reserved keyword
+        return SyntaxFacts.GetKeywordKind(param) == SyntaxKind.None && SyntaxFacts.IsValidIdentifier(param) ? param : $"@{param}";
+    }
 
     string CallParameters(IEnumerable<GdVariable> parameters)
     {
         parameters = parameters.ToList();
         if (!parameters.Any())
             return "";
-        return $", {string.Join(", ", parameters.Select(p => Pascalize(p.Name)))}";
+        return $", {string.Join(", ", parameters.Select(p => Pascalize(SanitizeParameter(p.Name))))}";
     }
-    
+
     string GetTypeCast(GdType type)
     {
         var typeString = type.ToCSharpTypeString(availableTypes);
         if (typeString is "Variant" or "void")
             return "";
-        
+
         return $".As<{typeString}>()";
     }
 
